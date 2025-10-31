5e8a94081db8a6b1371c6b554767eeb65e9b6819
Ajani Henry <henryajani.ah@gmail.com>
2025-10-31 18:39:49 -0500

    Added JavaScript validation for Newsletter email entry.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+
+    const newsletterform = document.querySelector('.newsletter form');
+    const emailaddress = newsletterform.querySelector('input[type="email"]');
+    const messagediv = document.querySelector('.message');
+
+    newsletterform.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        let email = emailaddress.value.trim();
+        if(!email){
+            messagediv.textContent = "Please enter a valid email address.";
+        } else{
+            messagediv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+        }
+    });
+};
